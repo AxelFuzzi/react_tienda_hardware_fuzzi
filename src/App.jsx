f44a9d5a8ee95d4aab9c8dabc5b6a53a3f6101d5
Axel Fuzzi <axelfuzziii@gmail.com>
2022-08-24 21:15:21 -0300

    counter_error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { Navbar } from "./components/Nav/Navbar";
 import { ItemListContainer } from "./components/itemListContainer/itemListContainer";
+import Counter from "./components/counter/Counter";
 
 export const App = () => {
     const saludo = "Bienvenidos"
+
+    const stock = (param) =>{
+        return(
+            param
+        )
+    };
+
+    const initial = (param) =>{
+        return(
+            param
+        )
+    };
+
+    const onAdd = alert("Gracias por su compra!");
     return (
         <>
             <Navbar/>
             <ItemListContainer saludo={saludo}/>
+            <Counter stock={10} initial={1} onAdd={onAdd}/>  
         </>
     );
 };
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+const Counter = ({stock, initial, onAdd}) => {
+    const [count, setCount] = useState(0)
+
+    const sumar = () =>{
+        if (count < stock){
+            setCount(count + 1);
+        }else{
+            alert("maximo stock");
+        }    
+    };
+
+    const restar = () =>{
+        if (count >= initial){
+            setCount(count - 1);
+        }else{
+            setCount(count + 0);
+        }
+    };
+  return (
+    <div>
+        <button onClick={sumar}>+</button>
+        <button onClick={restar}>-</button>
+        <p>Count: {count}</p>
+        <button disabled={count === 0} onClick={() => {onAdd}}>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default Counter
